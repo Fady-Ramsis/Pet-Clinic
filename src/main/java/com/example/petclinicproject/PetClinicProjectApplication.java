f860bbebb536ea2f4b6c737614839af6f404f867
fadyramsis <fadyrmais0@gmail.com>
2021-03-19 12:42:29 +0200

    Initial project

--- src/main/java/com/example/petclinicproject/PetClinicProjectApplication.java
+++ src/main/java/com/example/petclinicproject/PetClinicProjectApplication.java
@@ -8,6 +8,7 @@ public class PetClinicProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicProjectApplication.class, args);
+		System.out.println("asdasd");
 	}
 
 }
