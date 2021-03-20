3fdfd393cdd6ef9df42b41a98297d02f0ccf4700
fadyramsis <fadyrmais0@gmail.com>
2021-03-20 18:07:54 +0200

    Implement Base Entity

--- pet-clinic-data/src/main/java/com/example/petclinicproject/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/petclinicproject/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.petclinicproject.model;
 
-public class Person {
+public class Person extends BaseEntity{
     private String firstName;
     private String lastNamem;
 
--- pet-clinic-data/src/main/java/com/example/petclinicproject/model/PetType.java
+++ pet-clinic-data/src/main/java/com/example/petclinicproject/model/PetType.java
@@ -1,6 +1,6 @@
 package com.example.petclinicproject.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
     private String Name;
 
     public String getName() {
