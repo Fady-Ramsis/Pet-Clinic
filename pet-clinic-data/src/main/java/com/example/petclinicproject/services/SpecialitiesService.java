3404930e5491bcbeb936356fd278a653eadc30c6
fadyramsis <fadyrmais0@gmail.com>
2021-03-23 19:46:48 +0200

    Create Specialty Map Service

---
+++ pet-clinic-data/src/main/java/com/example/petclinicproject/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package com.example.petclinicproject.services;
+
+import com.example.petclinicproject.model.Speciality;
+
+public interface SpecialitiesService  extends CrudService<Speciality,Long> {
+}
